90d8213e7d0c0b06ad6cdbd50c4df489d7564199
Kent Fredric <kentfredric@gmail.com>
2017-03-20 19:09:59 +1300

    Factor out Yaml Assertions

--- maint-check.pl
+++ maint-check.pl
@@ -6,6 +6,7 @@ use FindBin;
 use lib "$FindBin::Bin/lib";
 
 use My::Assert::Path;
+use My::Assert::YAML;
 
 use Git::Wrapper ();
 
@@ -105,64 +106,6 @@ for ( path->with('resources') ) {
 }
 
 use My::Generic::Assertions;
-
-BEGIN {
-
-    package My::Assert::YAML;
-    use YAML::XS qw();
-    use Data::DPath qw( dpath );
-    use Term::ANSIColor qw( colored );
-
-    our $ASSERT_YAML = My::Generic::Assertions->new(
-        '-input_transformer' => \&transform_input,
-        '-tests'             => { have_dpath => \&test_have_dpath },
-        '-get_handler'       => \&handle_get,
-    );
-
-    sub assert { $ASSERT_YAML }
-
-    sub transform_input {
-        My::Assert::Path::assert->must( exist => $_[1] );
-        return (
-            {
-                yaml => YAML::XS::Load(
-                    My::Assert::Path::assert->get( 'utf8', $_[1] )
-                ),
-                file => $_[1]
-            },
-            @_[ 2 .. $#_ ]
-        );
-    }
-
-    sub handle_get {
-        my ( $name, @args ) = @_;
-        my $coderef = __PACKAGE__->can( 'get_' . $name );
-        die "No such getter get_$name" unless $coderef;
-        $coderef->(@args);
-    }
-
-    sub handle_should {
-        my ( $status, $message, $name, @slurpy ) = @_;
-        my $cname    = colored( ['yellow'], $name );
-        my $cmessage = colored( ['yellow'], $message );
-        warn "should $cname > $cmessage\n" unless $status;
-        return $status;
-    }
-    use Data::Dump qw(pp);
-
-    sub test_have_dpath {
-        my ( $data, $path ) = @_;
-        my (@results) = get_dpath( $data, $path );
-        return ( 0, "$data->{file} does not have dpath $path" ) unless @results;
-        return ( 1, "$data->{file} has dpath $path" );
-    }
-
-    sub get_dpath {
-        my ( $data, $path ) = @_;
-        Data::DPath->match( $data->{yaml}, $path );
-    }
-
-}
 
 BEGIN {
 
