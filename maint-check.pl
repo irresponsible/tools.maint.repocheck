f25e45195b4850373d71c784c1a60b0995bf6f9f
Kent Fredric <kentfredric@gmail.com>
2017-03-20 19:06:52 +1300

    Factor out path asserts

--- maint-check.pl
+++ maint-check.pl
@@ -5,6 +5,8 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 
+use My::Assert::Path;
+
 use Git::Wrapper ();
 
 our $BOOT_VERSION    = '2.7.1';
@@ -103,66 +105,6 @@ for ( path->with('resources') ) {
 }
 
 use My::Generic::Assertions;
-
-BEGIN {
-
-    package My::Assert::Path;
-    use Path::Tiny;
-
-    our $ASSERT_PATH = My::Generic::Assertions->new(
-        '-input_transformer' => \&transform_input,
-        -tests               => {
-            exist              => \&test_exist,
-            have_line_matching => \&test_have_line_matching,
-        },
-        '-get_handler' => \&handle_get,
-    );
-    sub assert { $ASSERT_PATH }
-
-    our %linecache;
-
-    sub _getlines {
-        @{
-            exists $linecache{ $_[0] }
-            ? $linecache{ $_[0] }
-            : $linecache{ $_[0] } = [ $_[0]->lines_raw( { chomp => 1 } ) ]
-        };
-    }
-
-    sub transform_input {
-        my ( $name, $path, @rest ) = @_;
-        return ( Path::Tiny::path($path), @rest );
-    }
-
-    sub test_exist {
-        return ( 0, "$_[0] does not exist" ) unless $_[0]->exists;
-        return ( 1, "$_[0] exists" );
-    }
-
-    sub test_have_line_matching {
-        my $re = $_[1];
-        my $smessage = !$_[2] ? "" : " ( $_[2] )";
-        return ( 0, "$_[0] does not have line matching ${re}${smessage}" )
-          unless grep { $_ =~ $re } _getlines( $_[0] );
-        return ( 1, "$_[0] has line matching ${re}${smessage}" );
-    }
-
-    sub handle_get {
-        my ( $name, @args ) = @_;
-        my $coderef = __PACKAGE__->can( 'get_' . $name );
-        die "No such getter get_$name" unless $coderef;
-        $coderef->(@args);
-    }
-
-    sub get_lines {
-        _getlines( $_[0] );
-    }
-
-    sub get_utf8 {
-        $_[0]->slurp_utf8;
-    }
-
-}
 
 BEGIN {
 
